873318254992aa3e3bcb7f4ed3ccdf6a53ff6dec
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-02-10 20:18:23 +0200

    api token put in env and env in gitIgnore

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { Picker } from "@react-native-picker/picker";
 import { dummyapidata } from "./dummyData";
 import { StatusBar } from "expo-status-bar";
 import { useRef, useState, useEffect } from "react";
+import { API_TOKEN } from "@env";
 import {
   Button,
   StyleSheet,
@@ -15,13 +16,8 @@ import {
 export default function App() {
   const [selectedCurrency, setSelectedCurrency] = useState("");
   const [amount, setAmount] = useState("");
-  // const [apiData, setApiData] = useState()
   const [result, setResult] = useState();
-  // const [curs, setCurs] = useState();
   const [availableCur, setAvailableCur] = useState({});
-
-
-
 
   const pickerRef = useRef();
   function open() {
@@ -32,16 +28,14 @@ export default function App() {
   }
 
   const count = () => {
-
-    if (!availableCur)return;
+    if (!availableCur) return;
     let r = availableCur[selectedCurrency];
-    let calc = amount / r
-     setResult(Number.parseFloat(calc).toFixed(2));
- 
+    let calc = amount / r;
+    setResult(Number.parseFloat(calc).toFixed(2));
   };
 
   var myHeaders = new Headers();
-  myHeaders.append("apikey", "adunRcpgGApFOGxfXsSG7EoCQ7sXfhSj");
+  myHeaders.append("apikey", API_TOKEN);
 
   var requestOptions = {
     method: "GET",
@@ -50,14 +44,16 @@ export default function App() {
   };
 
   useEffect(() => {
-    fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=GBP%2CJPY%2CUSD&base=EUR", requestOptions)
-    .then(response => response.json())
-    .then(responseJson => {
-      setAvailableCur(responseJson.rates)
-      console.log(Object.keys(responseJson.rates) + ' in useeffect')
-    })
-  },[])
-
+    fetch(
+      "https://api.apilayer.com/exchangerates_data/latest?symbols=GBP%2CJPY%2CUSD&base=EUR",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setAvailableCur(responseJson.rates);
+        console.log(Object.keys(responseJson.rates) + " in useeffect");
+      });
+  }, []);
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
@@ -78,9 +74,11 @@ export default function App() {
             setSelectedCurrency(itemValue)
           }
         >
-        {availableCur && Object.keys(availableCur).map(cur => (<Picker.Item label={cur} value={cur} />))}
-
-        </Picker> 
+          {availableCur &&
+            Object.keys(availableCur).map((cur) => (
+              <Picker.Item label={cur} value={cur} />
+            ))}
+        </Picker>
 
         <Button title="convert" onPress={count}></Button>
 
@@ -89,7 +87,6 @@ export default function App() {
     </TouchableWithoutFeedback>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
